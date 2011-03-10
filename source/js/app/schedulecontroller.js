5a577263f2044a73b17fcb94e3b2f03fa07960e8
Tommy Klumker <tommy@smithandrobot.com>
2011-03-10 10:49:11 -0600

    schedule shizz

--- source/js/app/schedulecontroller.js
+++ source/js/app/schedulecontroller.js
@@ -4,7 +4,21 @@ ScheduleController.constructor = ScheduleController;
 function ScheduleController() 
 {
     var model 		= new TRModel();
-    var feedServer  = ''
+    var feedURL  = 'mock/schedule.json'
+    
+    model.addEventListener('onDataChange', onDataChange);
+    loadFeed();
+    
+    function onDataChange( e )
+	{
+		Log('New Data');
+	}
+	
+	function loadFeed(  )
+	{
+		model.setStream( feedURL );
+		model.load();
+	}
     
 	return this;
 };
