b7a2a78ef4a5874c952fa644f9f11624e7eaf943
Tommy Klumker <tommy@smithandrobot.com>
2011-03-10 12:33:03 -0600

    more schedule stuff

--- source/js/app/teammodel.js
+++ source/js/app/teammodel.js
@@ -1,10 +1,29 @@
 Teams = (function Teams() 
 {
 	var team_schema = {
-        '1234' : {
-            'display_name' : 'Tarheels',
+        '12234' : {
+            'displayName' : 'Hilltoppers',
             'color'        : '#97B7E0',
-    		'feed'		   : ''
+    		'feed'		   : '',
+    		'hashTag'      : '#HillToppers'
+        },
+        '123' : {
+            'displayName' : 'Wildcats',
+            'color'        : '#97B7E0',
+    		'feed'		   : '',
+    		'hashTag'      : '#Wildcats'
+        },
+        '1234' : {
+            'displayName' : 'Mean Green',
+            'color'        : '#97B7E0',
+    		'feed'		   : '',
+    		'hashTag'      : '#Grean'
+        },
+        '12345' : {
+            'displayName' : 'Bruins',
+            'color'        : '#97B7E0',
+    		'feed'		   : '',
+    		'hashTag'      : '#Bruins'
         }
     }
     
