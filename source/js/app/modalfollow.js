a3f7f1f37a7737626d5913d3a30b5c035725159a
David Ford <david@smithandrobot.com>
2011-03-11 14:37:02 -0600

    removed html from index page, added redirect code also created app.html and callback.html for twitter app

---
+++ source/js/app/modalfollow.js
@@ -0,0 +1,90 @@
+ModalFollow.prototype 	= new EventDispatcher();
+ModalFollow.constructor 	= ModalFollow;
+
+function ModalFollow( overlay ) 
+{
+	var element 	= $('#modal-follow-template');
+	var overlay		= overlay;
+	var img			= null;
+	var rendered	= false;
+	var state		= 'closed';
+	
+	this.tweet		= null;
+	this.open 	 	= open;
+	
+	overlay.addEventListener('onModalOverlayClose', onClose)
+	decorateBTNS();
+	initCSS();
+	
+	
+	function open( tweet )
+	{
+		if( tweet ) setContent( tweet );
+		
+		if( state == 'closed') 
+		{
+			element.css('z-index', overlay.z+1)
+			element.fadeIn(250);
+			overlay.open();
+		}
+		
+		position();
+		state = 'open';
+	}
+	
+	
+	function onClose()
+	{
+		element.fadeOut(250);
+		overlay.close();
+		state = 'closed';
+	}
+	
+	function setContent( t )
+	{
+		var html = t.getElement().clone();
+
+		element.find('.modal-dialog').append().html('Start following<br /><span class="red">'+t.screenName+'</span>')
+	}
+	
+	function position( animate )
+	{				
+    	var docY = ($(window).height() / 2) - 100;
+    	var docX = $(document).width() / 2;	
+		var height = element.height() / 2;
+		var width = element.width() / 2;
+		var top = (docY - height < 0) ? 10 : docY - height;
+		var left = docX - width;
+		
+		var propObject = {};
+		propObject.left = left;
+		propObject.top = top;
+		if(animate)
+		{ 
+			element.animate(propObject, 250);
+		}else{
+			element.css(propObject);
+		}
+	}
+
+	function decorateBTNS()
+	{
+		
+		var cBtn = element.find('.close-button');
+		cBtn.click(onClose);
+		cBtn.hover(function() {$(this).css('cursor','pointer')}, function() {$(this).css('cursor','auto')} );
+		
+		var cancelBtn = element.find('.modal-cancel-button');
+		cancelBtn.click(onClose);
+		cancelBtn.hover(function() {$(this).css('cursor','pointer')}, function() {$(this).css('cursor','auto')} );
+		
+	}
+	
+	function initCSS()
+	{
+		element.css('position', 'absolute');
+		position( false );
+	}
+	
+	return this;
+};
