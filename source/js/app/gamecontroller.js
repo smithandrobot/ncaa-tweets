70edbdee20a598ed1bd8995eb0426d95d4e1d807
Tommy Klumker <tommy@smithandrobot.com>
2011-03-16 13:19:20 -0500

    game controller fix to handle overtime and delayed games

--- source/js/app/gamecontroller.js
+++ source/js/app/gamecontroller.js
@@ -131,39 +131,46 @@ function GameController(gameData, model)
         }
 
     }
-    
-    function setTips(template, viewData){
+
+    function setTips(template, viewData) {
         tipContent = '<div class="hovercard" tid="v">Click to view Tweets for<br/> ' + template.find('.team-visiting .team-name').text() + '</div>'
         tipObj = {
             content: tipContent,
             show: 'mouseover',
-            hide: { when: 'mouseout', fixed: true, delay:250 },
+            hide: {
+                when: 'mouseout',
+                fixed: true,
+                delay: 250
+            },
             style: {
                 tip: false,
-                cursor:'pointer',
+                cursor: 'pointer',
                 width: 250,
                 height: 79,
-                'background':'none',
-                color:'#fff',
-                'text-align':'center',
-                'font-size':'11px',
-                'padding-right':'10px',
-                border:'none',
+                'background': 'none',
+                color: '#fff',
+                'text-align': 'center',
+                'font-size': '11px',
+                'padding-right': '10px',
+                border: 'none',
                 classes: {
-                    content:'.tweet-cta-tip'
+                    content: '.tweet-cta-tip'
                 }
             },
             position: {
                 corner: {
                     target: 'topMiddle',
                     tooltip: 'bottomMiddle'
-                }, 
-                adjust: { x: 12, y: 0 }
+                },
+                adjust: {
+                    x: 12,
+                    y: 0
+                }
             },
             api: {
-                onRender:function(){
-                    var tid=this.elements.content.find('*[tid]').attr('tid')
-                    if(tid == 'v'){
+                onRender: function() {
+                    var tid = this.elements.content.find('*[tid]').attr('tid')
+                    if (tid == 'v') {
                         var t = self.visitingTeam
                     } else {
                         var t = self.homeTeam
@@ -175,17 +182,17 @@ function GameController(gameData, model)
                 }
             }
         }
-        if(self.visitingTeam.name != "TBA"){
+        if (self.visitingTeam.name != "TBA") {
             template.find('.visiting-tweet-cta').qtip(tipObj)
         } else {
             template.find('.visiting-tweet-cta').hide()
         }
-        
+
         tipObj.content = '<div class="hovercard" tid="h">Click to view Tweets for<br/> ' + template.find('.team-home .team-name').text() + '</div>'
         template.find('.home-tweet-cta').qtip(tipObj)
     }
-    
-    
+
+
     function sanitizeData(data) {
 
         var templateData = {
@@ -236,13 +243,17 @@ function GameController(gameData, model)
             templateData.game.status = "final"
             templateData.game.period = "final"
             self.active = false
-        } else if (data.eventstatus.status == "SCHEDULED") {
+        } else if (data.eventstatus.status == "SCHEDULED" || data.eventstatus.status == "DELAYED") {
             templateData.game.status = "scheduled"
             templateData.game.period = templateData.date.toString("MMM d, h:mm tt") + " EST";
             self.active = false
         } else if (data.eventstatus.status == "INTERMISSION") {
             templateData.game.status = "current"
             templateData.game.period = "Half"
+            self.active = true
+        } else if (data.eventstatus.status == "OVERTIME") {
+            templateData.game.status = "current"
+            templateData.game.period = "OT"
             self.active = true
         } else {
             templateData.game.status = "current"
@@ -262,7 +273,7 @@ function GameController(gameData, model)
         }
 
     }
-    
+
 
     function hashTagClick(obj) {
         self.hashTag = $(this).text();
